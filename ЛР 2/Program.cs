1802610b2caca3d2031deb7dd11d2d80fb420b5e
DraftIn25 <SbVViT0512@mail.ru>
2023-10-30 22:53:45 +1000

    LR2Commit

---
+++ ЛР 2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ЛР_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text=null;
+            Console.WriteLine("Программа по решению квадратного уравнения.\nОбщий вид квадратного уравнения: ax^2+bx+c=0");
+            try
+            {
+                StreamReader reader = new StreamReader("H:/Тестирование и отладка ПО/ЛР 2/ЛР 2/Parametres.txt");
+                 text = reader.ReadToEnd();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Ошибка: "+ex.Message);
+            }
+            string [] data=text.Split(' ');
+            try
+            {
+                for (int i = 0; i < data.Length; i++)
+                    if (data[i].Length<1|| Regex.IsMatch(data[i], @"^[a-zA-Z]+$"))
+                        throw new ParametresInFileException("Входные данные имеют неверный формат");
+            }
+            catch(ParametresInFileException ex)
+            {
+                Console.WriteLine("Ошибка: "+ex.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("Данные успешно получены!");
+            }
+            double a = Convert.ToDouble(data[0]);
+            double b = Convert.ToDouble(data[1]);
+            double c = Convert.ToDouble(data[2]);
+            Quadratic_Equation quadratic = new Quadratic_Equation(a, b, c);
+            try
+            {
+                quadratic.Show();
+            }
+            catch (aException ex) 
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            try 
+            {
+                quadratic.Solution();
+            }
+            catch(NegativeDiscriminantException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ЛР 2/Quadratic Equation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР_2
+{
+    internal class Quadratic_Equation
+    {
+        double a, b, c;
+        public Quadratic_Equation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void Show()
+        {
+            string text = "";
+            if (a != 0)
+                text = $"{a}x^2";
+            if (b > 0)
+                text = text + $"+{b}x";
+            else if (b == 0)
+                text = text + "";
+            else
+                text = text + $"{b}x";
+            if (c > 0)
+                text = text + $"+{c}x";
+            else if (c == 0)
+                text = text + "";
+            else
+                text = text + $"{c}x";
+            if (a == 0)
+                throw new aException($"Получили линейное уравнение {text}=0.Введите параметр 'a'>0");
+            else
+            Console.WriteLine($"Квадратное уравнение:{text}=0");
+        }
+        public void Solution()
+        {
+            double d = b * b - 4 * a * c;
+            Console.WriteLine("Дискриминант = "+d);
+            if (d < 0)
+                throw new NegativeDiscriminantException("Дисрриминант < 0. Нет решений");
+            double x1=(-b+Math.Sqrt(d))/(2*a);
+            double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+            if(d==0)
+                Console.WriteLine($"Уравнение имеет один корень:x1={x1}");
+            else if(d>0)
+            Console.WriteLine($"Уравнение имеет два корня x1={Math.Round(x1,3)} x2={Math.Round(x2,3)}");
+        }
+    }
+}
